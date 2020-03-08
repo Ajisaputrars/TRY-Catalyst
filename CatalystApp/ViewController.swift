172184c193a4e3f2e1f9e5743df71e491715aa5a
Aji Saputra Raka Siwi <ajisaputrars@gmail.com>
2020-03-08 14:22:29 +0700

    Initial Commit

---
+++ CatalystApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatalystApp
+//
+//  Created by Aji Saputra Raka Siwi on 08/03/20.
+//  Copyright © 2020 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CatalystAppTests/CatalystAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  CatalystAppTests.swift
+//  CatalystAppTests
+//
+//  Created by Aji Saputra Raka Siwi on 08/03/20.
+//  Copyright © 2020 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import XCTest
+@testable import CatalystApp
+
+class CatalystAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
